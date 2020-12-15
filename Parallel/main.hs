87391dc353e47fb0fb651b992cf90ecf913f2288
Shengkai Li <sl4685@columbia.edu>
2020-12-15 01:26:39 -0500

    main() for test only

--- Parallel/main.hs
+++ Parallel/main.hs
@@ -1,13 +1,14 @@
 import NgramCount ( par_mapper, par_reducer )
 import WordClean ( wordClean )
-
+import Suggestion_Parallel ( par_suggestion_filer, suggestion_sort, suggestion_filer ) 
 import System.Environment ( getArgs )
 
 
 main :: IO ()
 main = do
-    [n, filename] <- getArgs
+    [n, filename, str] <- getArgs
     content <- wordClean filename
     let input = map words $ lines content
     let output = par_reducer $ par_mapper (read n) input
-    writeFile "Ngram.txt" (show output)
+    print $ head $ suggestion_sort $ par_suggestion_filer str output
+    --print $ head $ suggestion_sort $ suggestion_filer str output
